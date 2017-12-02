17a2371c3cfb69cfe7b6ef1ea06f388ee40d3e04
Eugene Pavlov <bjohn.ext@gmail.com>
2017-12-02 23:58:02 +0300

    add GirlController

---
+++ Assets/GirlController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GirlController : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+
+    private Plane[] planes;
+    private Collider2D collider;
+
+    void Start()
+    {
+        var cam = FindObjectOfType<Camera>();
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.localPosition;
+        pos.x += velocity * Time.deltaTime;
+        transform.localPosition = pos;
+
+        
+        if (!GeometryUtility.TestPlanesAABB(planes, collider.bounds))
+        {
+            Debug.LogFormat("hide");
+            velocity *= -1;
+            pos.x += velocity * Time.deltaTime;
+            transform.localPosition = pos;
+
+            var scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
